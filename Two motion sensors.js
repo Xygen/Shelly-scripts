45dfc63c460f39c984b6160fe3794fc52b0af82f
Xygen <xygen75@icloud.com>
2025-06-04 11:08:54 +0200

    fix typo in introductory comment

--- Two motion sensors.js
+++ Two motion sensors.js
@@ -1,6 +1,6 @@
 / This script is for using 2 (two) Shelly BLU motion sensors triggering a Shelly 1PM mini Gen3 relais.
 / The difference to the original script from the Shelly Library called "BLE in Scripting - Shelly BLU
-/ Motion script actions" is that the relais turnes off only when both motion sensors report "no motion".
+/ Motion script actions" is that the relais turns off only when both motion sensors report "no motion".
 / In the original script one motion sensor reporting "no motion" turned the relais off. 
 / The part after "STOP CHANGE HERE" is identical to the original script
 / (https://github.com/ALLTERCO/shelly-script-examples/blob/main/ble-shelly-motion.js)! 
